72233c2facbb5a69fdc9d37cfaabc111cbaf9bf1
RIYAJ MOHAMMAD <mdriyaj82084@gmail.com>
2024-10-22 22:03:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    document.getElementById('form-response').innerText = `Thank you, ${name}! Your message has been received.`;
+
+    document.getElementById('contact-form').reset();
+});
